820a30b3f2f1079d3b0c84f06d82a706767eb252
islem-abbassi <islemabbassi5090@gmail.com>
2025-08-01 17:55:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// let count=0
+// let countI=document.getElementById("counti")
+// function increment(){
+//   count=count+1;
+//   countI.innerText=count;
+// }
+
+
+// function save(){
+//     let saved=count+"-"
+// document.getElementById("saving").innerText+=saved
+// count=0
+// countI.innerText=count;
+// }
+
+function tryit()
+{
+    document.getElementById("trying").textContent="soory babe"
+}
+

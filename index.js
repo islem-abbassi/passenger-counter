cfbd35b97553402049fff08da7cac92f630a55ba
islem-abbassi <islemabbassi5090@gmail.com>
2025-08-01 17:58:10 +0100

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// let count=0
-// let countI=document.getElementById("counti")
-// function increment(){
-//   count=count+1;
-//   countI.innerText=count;
-// }
+ let count=0
+ let countI=document.getElementById("counti")
+ function increment(){
+   count=count+1;
+   countI.innerText=count;
+ }
 
 
-// function save(){
-//     let saved=count+"-"
-// document.getElementById("saving").innerText+=saved
-// count=0
-// countI.innerText=count;
-// }
-
-function tryit()
-{
-    document.getElementById("trying").textContent="soory babe"
-}
-
+ function save(){
+     let saved=count+"-"
+ document.getElementById("saving").innerText+=saved
+ count=0
+ countI.innerText=count;
+ }
